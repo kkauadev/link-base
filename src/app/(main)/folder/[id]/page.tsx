736d68301f22284c02aed2cd7571dfa524c52722
Kato2004 <kauaalvesnascimento22@gmail.com>
2023-11-05 23:38:35 -0300

    feat: added search box in folder/[id] route

--- src/app/(main)/folder/[id]/page.tsx
+++ src/app/(main)/folder/[id]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { CgSearch as IconSearch } from "react-icons/cg";
 import Cookies from "js-cookie";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
-import { memo, useState } from "react";
+import { memo, useEffect, useState } from "react";
 
 import { LinkCard } from "@/components/cards/link-card";
 import { IconDelete, IconPlus } from "@/components/icons";
@@ -46,6 +47,8 @@ const ActionButton = ({ children, className, href }: ActionButtonProps) => {
 export default function FolderPage() {
   const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
   const { id: routeParameterId } = useParams();
+  const [dataLinks, setDataLinks] = useState<Folder["links"]>([]);
+  const [toggleInputSearch, setToggleInputSearch] = useState(false);
   const { push } = useRouter();
   const cookieId = Cookies.get("id");
   const cookieToken = Cookies.get("token");
@@ -55,7 +58,23 @@ export default function FolderPage() {
     cookieToken ?? ""
   );
 
-  console.log(data);
+  useEffect(() => {
+    if (data?.links) {
+      setDataLinks(data?.links);
+    }
+  }, [data?.links]);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!data?.links) return;
+    const searchTerm = e.target.value.toLowerCase();
+
+    if (searchTerm.trim() === "") return setDataLinks(data?.links);
+
+    const dataFiltered = data?.links.filter((folder) =>
+      folder.title.toLowerCase().includes(searchTerm)
+    );
+    setDataLinks(dataFiltered);
+  };
 
   const MemoizedLinkCard = memo(LinkCard);
 
@@ -90,25 +109,45 @@ export default function FolderPage() {
             Pasta {data.name}
           </h1>
           <section className="flex flex-col-reverse items-center lg:items-stretch lg:flex-row gap-4 mt-6 sm:mt-10 mb-5">
-            <ul className="flex flex-col gap-4 w-full sm:pr-6">
-              {data.links && data.links.length === 0 ? (
-                <li className="text-xl">
-                  Nenhum link adicionado a pasta <strong>{data.name}</strong>{" "}
-                  ainda
-                </li>
-              ) : (
-                data?.links?.map((link) => {
-                  return (
-                    <MemoizedLinkCard
-                      key={link.id}
-                      link={link}
-                      paramsId={routeParameterId}
-                      stored={{ token: cookieToken ?? "", id: cookieId ?? "" }}
-                    />
-                  );
-                })
-              )}
-            </ul>
+            <div className="flex flex-col gap-4 w-full">
+              <div className="flex gap-4 h-10">
+                <button
+                  onClick={() => setToggleInputSearch((e) => !e)}
+                  className="text-2xl hover:brightness-50 transition-all duration-200"
+                >
+                  <IconSearch />
+                </button>
+                {toggleInputSearch ? (
+                  <input
+                    onChange={handleSearch}
+                    className="bg-tertiary outline-none border-2 border-secondary active:border-gray-500  focus:border-gray-500 rounded px-2 py-1 w-[20rem]"
+                    type="text"
+                  />
+                ) : null}
+              </div>
+              <ul className="flex flex-col gap-4 w-full sm:pr-6">
+                {dataLinks && dataLinks.length === 0 ? (
+                  <li className="text-xl">
+                    Nenhum link adicionado a pasta <strong>{data.name}</strong>{" "}
+                    ainda
+                  </li>
+                ) : (
+                  dataLinks.map((link) => {
+                    return (
+                      <MemoizedLinkCard
+                        key={link.id}
+                        link={link}
+                        paramsId={routeParameterId}
+                        stored={{
+                          token: cookieToken ?? "",
+                          id: cookieId ?? "",
+                        }}
+                      />
+                    );
+                  })
+                )}
+              </ul>
+            </div>
             <aside className="max-w-md w-full">
               <section className="flex justify-between items-center gap-4 mb-4">
                 <ActionButton
